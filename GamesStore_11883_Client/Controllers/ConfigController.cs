9be0e43d1df70efe25fa4021327d0f19f74dce90
00011883 <wiut00011883@gmail.com>
2023-10-31 02:16:24 +0500

    feat: MVC .net framework app v4.7.2

---
+++ GamesStore_11883_Client/Controllers/ConfigController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace GamesStore_11883_Client.Controllers
+{
+    public class ConfigController
+    {
+        public readonly string baseurl = "http://localhost:32768";
+    }
+}
