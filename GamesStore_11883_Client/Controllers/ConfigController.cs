7a875e5a564e00d135cb1c936a7e780062961003
00011883 <91319484+00011883@users.noreply.github.com>
2023-11-08 00:54:40 +0500

    feat: update deployed api endpoint for viva

--- GamesStore_11883_Client/Controllers/ConfigController.cs
+++ GamesStore_11883_Client/Controllers/ConfigController.cs
@@ -9,7 +9,7 @@ namespace GamesStore_11883_Client.Controllers
     public class ConfigController
     {
         // PROD env url
-        public readonly string baseurl = "http://ec2-54-193-21-173.us-west-1.compute.amazonaws.com";
+        public readonly string baseurl = "http://ec2-13-52-231-34.us-west-1.compute.amazonaws.com";
 
         // DEV env url
         // public readonly string baseurl = "http://localhost:32768";
